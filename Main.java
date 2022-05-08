b5c7db0a10af45d059101875db49c4857306421d
SaadJaved23 <105151890+SaadJaved23@users.noreply.github.com>
2022-05-08 15:53:36 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,7 +20,7 @@ public class Main {
         System.setProperty("webdriver.chrome.driver","/Users/saadjaved/Downloads/Selenium jave/chromedriver");
 
         WebDriver driver = new ChromeDriver();
-        //WebDriver driver = new FirefoxDriver();
+        
         driver.get("https://quixel.com/megascans/home/");
 
         driver.findElement(By.xpath("//*[@id='app-page']/div/div[1]/div/ul/li[4]/a")).click();
@@ -30,8 +30,7 @@ public class Main {
         driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[2]/div/div/section[1]/div/a[4]/span/div/div/div/div[2]/div")).click();
         driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[2]/div/div/section[2]/div/div[1]/div[3]/a/span/div/div/span")).click();
         driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/div[2]/div/button")).click();
-        //driver.findElement(By.className("css-xwv3p2")).click();
-        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+       
         driver.findElement(By.className("_3PNMHbJE22xaFXrB596K3i")).click();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div/div[2]/div/div[1]/ul/div[1]/span")).click();
